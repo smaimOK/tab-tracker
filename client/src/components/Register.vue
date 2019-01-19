e40d8454158d1bed645f645403a2ec9574bd80c1
snaimOK <serepuga@yandex.ru>
2019-01-19 17:12:27 +0800

    setting up registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input
+      type="email"
+      name="emali"
+      v-model="email"
+      placeholder="emal"/>
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password">
+    <br>
+    <button
+      @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      console.log(this.email, this.password)
+    }, 2000)
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
