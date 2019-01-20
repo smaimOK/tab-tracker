a9b10a89d0c0f6667010b49dba05bb175f380e36
snaimOK <serepuga@yandex.ru>
2019-01-20 16:35:32 +0800

    connecting to sqlite using sequelize and freshening up the register page

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -5,10 +5,3 @@ export default {
     return Api().post('register', credentials)
   }
 }
-
-/*
-AuthenticationService.register({
-  email: 'testing@gmail.com',
-  password: '123456'
-})
-*/
--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,19 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const { sequelize } = require('./models')
+const config = require('./config/config')
+
 const app = express()
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
-  })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8080)
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Serever started on port ${config.port}`)
+  })
